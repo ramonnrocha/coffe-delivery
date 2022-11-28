0f87bdfb4dae6ac30832852a36c17728e1ef5395
Ramonn <ramonnsantana@gmail.com>
2022-11-28 18:03:16 -0300

    Version 3

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,36 @@
-import { DivContainer, HeaderContainer } from "./styles."
-
-import logo from "../../assets/logo.svg"
-import { MapPin, ShoppingCart } from "phosphor-react"
+import {
+  HeaderButton,
+  HeaderButtonsContainer,
+  HeaderContainer,
+} from "./styles";
+import coffeeLogoImg from "../../assets/coffee-delivery-logo.svg";
+import { MapPin, ShoppingCart } from "phosphor-react";
+import { NavLink } from "react-router-dom";
+import { useCart } from "../../hooks/useCart";
 
 export function Header() {
+  const { cartQuantity } = useCart();
+
   return (
     <HeaderContainer>
-      <img src={logo} alt="logo" />
-      <DivContainer>
-        <button className="location">
-          <MapPin size={22} weight="fill" />
+      <div className="container">
+        <NavLink to="/">
+          <img src={coffeeLogoImg} alt="" />
+        </NavLink>
 
-          Porto Alegre, RS
-        </button>
-        <button className="cart">
-          <ShoppingCart size={22} weight="fill" />
-        </button>
-      </DivContainer>
+        <HeaderButtonsContainer>
+          <HeaderButton variant="purple">
+            <MapPin size={20} weight="fill" />
+            Porto Alegre, RS
+          </HeaderButton>
+          <NavLink to="/completeOrder">
+            <HeaderButton variant="yellow">
+              {cartQuantity >= 1 && <span>{cartQuantity}</span>}
+              <ShoppingCart size={20} weight="fill" />
+            </HeaderButton>
+          </NavLink>
+        </HeaderButtonsContainer>
+      </div>
     </HeaderContainer>
-  )
+  );
 }
